cea82a46f70666aade6f76fc6d1f615032196f4e
Jordan Lapid <jordanlapid@gmail.com>
2017-08-23 21:57:29 -0700

    sf changes

---
+++ assets/javascript/game.js
@@ -0,0 +1,18 @@
+
+
+//variables
+	//create variables for life counters (player 1, player 2)
+
+//jquery
+	//create attack function that decrements life counters
+	//create function to move div into player divs
+		//https://stackoverflow.com/questions/4291151/jquery-count-child-elements
+			//first click moves into player 1 div
+			//second click moves into player 2 div
+	//conditional statement to check if there are two players
+		//if there are two players selected, then the attack function works
+	//conditional to check life
+		//if player 1 life drops to 0, then game over
+		//if player 2 life drops to 0, then remove the div from the game
+	//conditional to check win condition
+		//if there are no opponents 
